4e3c32b153c345e10cee43da0856080360d97ff3
mariapujirahayu <mariapujir@gmail.com>
2019-10-21 08:29:51 +0700

    Menambahkan navbar

---
+++ galeri.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Galeri</title>
+    <link href="bootstrap-4.3.1-dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <?php include 'includes/navbar.php' ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta content="width=device-width, initial-scale=1" name="viewport">
-	<title>Responsive Bootstrap Theme</title>
+	<title>Cintya Wedding Organizer</title>
 	<link href="bootstrap-4.3.1-dist/css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
 </head>
