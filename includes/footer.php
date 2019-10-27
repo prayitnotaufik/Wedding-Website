40debd4d2314a35e7168ad6f597dcd4e53784121
Muhammad Taufik Prayitno <taufikprayitno16@gmail.com>
2019-10-27 14:54:39 +0700

    modifikasi footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,17 +1,17 @@
 <!--- Start Footer -->
 	<footer>
 		<div class="container">
-			<div class="row text-center py-5">
+			<div class="row text-center">
 				<div class="col-md-4">
-					<h3 class="text-center">ADDRESS</h3>
+					<h5 class="text-center">ADDRESS</h5>
 					<p>Jl. Kapt. Kyai Ilyas 31A Lumajang</p>
 				</div>
 				<div class="col-md-4">
-					<h3 class="text-center">CONTACT INFO</h3>
+					<h5 class="text-center">CONTACT INFO</h5>
 					<p>Cintyawedding@gmail.com</p>
 				</div>
 				<div class="col-md-4 pb-5">
-					<h3 class="text-center">CONNECT WITH US</h3>
+					<h5 class="text-center">CONNECT WITH US</h5>
 				</div>
 			</div><!--- End of Row -->
 		</div><!--- End of Container -->
