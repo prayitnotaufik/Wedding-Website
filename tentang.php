bb11ea8877e807f56ddec1c7334c01545fdb00ab
mariapujirahayu <mariapujir@gmail.com>
2019-10-27 21:48:20 +0700

    Mengedit halaman tentang

--- tentang.php
+++ tentang.php
@@ -1,8 +1,43 @@
 <!DOCTYPE html>
 <html>
-<?php include 'includes/head.php'?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Compact Gallery</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css" />
+    <link rel="stylesheet" href="css/grid-gallery.css">
+    <?php include 'includes/head.php'?>
+</head>
 <body>
     <?php include 'includes/navbar.php' ?>
+    <section class="gallery-block grid-gallery">
+            <div class="container">
+                <div class="heading pt-5">
+                    <h1 class="text-center">Tentang</h1>
+                </div>
+                <div class="column">
+                    <h3 style="color:rgb(255, 166, 109)">Tentang Cintya Wedding Organizer</h3>
+                    <p>Cintya wedding organizer merupakan salah satu upaya dalam membantu menyiapkan pesta pernikahan yang yang memakai web sebagai media promosi. Disini kami menyediakan berbagai paket pernikahan dengan harga terjangkau</p>
+                    <br>
+                    <h3 style="color:rgb(255, 166, 109)">Alamat</h3>
+                    <p>Jl. Kapt. Kyai Ilyas 31A Lumajang</p>
+                    <br>
+                    <h3 style="color:rgb(255, 166, 109)">Kontak Kami</h3>
+                    <p>
+                    <img src="img/emaill.png" alt="Email" style="float:left;width:30px;height:30px;">
+                    Cintyawedding@gmail.com
+                    </p>
+                    <p>
+                    <img src="img/logo_telp.png" alt="telp" style="float:left;width:30px;height:30px;">
+                    081227189023
+                    </p>
+                </div>
+            </div>
+        </section>
+
+
     <?php include 'includes/footer.php' ?>
 </body>
 
