e649ca873e595ee2e5e5b017cd067d53f1036a2d
Muhammad Taufik Prayitno <taufikprayitno16@gmail.com>
2019-10-29 17:20:38 +0700

    Tambah halaman pemesanan

---
+++ pemesanan.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'includes/head.php'?>
+<body>
+<?php include 'includes/navbar.php'?>
+    <div class="container">
+        <div class="row">
+            <h1></h1>
+        </div>
+    </div>
+<?php include 'includes/footer.php' ?>
+</body>
+</html>
