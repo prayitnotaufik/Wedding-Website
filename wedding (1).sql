97a663d361ffd41ef6d4c35c70e1267996df9cc7
Muhammad Taufik Prayitno <taufikprayitno16@gmail.com>
2019-11-25 08:54:50 +0700

    Database baru

--- wedding (1).sql
+++ wedding (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2019 at 09:25 AM
+-- Generation Time: Nov 25, 2019 at 02:52 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -25,14 +25,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `galeri`
+-- Table structure for table `galeri2`
 --
 
-CREATE TABLE `galeri` (
-  `id_gambar` int(11) NOT NULL,
-  `file_gambar` varchar(255) DEFAULT NULL,
-  `paket_id` int(11) NOT NULL
+CREATE TABLE `galeri2` (
+  `id_gambar` int(100) NOT NULL,
+  `file_gambar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `galeri2`
+--
+
+INSERT INTO `galeri2` (`id_gambar`, `file_gambar`) VALUES
+(2, 'gal1.png'),
+(3, 'gal2.png'),
+(4, 'gal3.png');
 
 -- --------------------------------------------------------
 
@@ -48,8 +56,17 @@ CREATE TABLE `paket` (
   `rias_baju` varchar(255) DEFAULT NULL,
   `dokumentasi` varchar(255) DEFAULT NULL,
   `mc` varchar(45) DEFAULT NULL,
-  `free` varchar(255) DEFAULT NULL
+  `free` varchar(255) DEFAULT NULL,
+  `foto` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `paket`
+--
+
+INSERT INTO `paket` (`id_paket`, `nama_paket`, `harga`, `dekorasi`, `rias_baju`, `dokumentasi`, `mc`, `free`, `foto`) VALUES
+(11, 'test', '12.000.000', 'test', 'asdasd', '', '', '', 'image6.jpg'),
+(12, 'Bougenville', '10.000.000', 'Panggung dan Emboh', 'pengantin, keluarga', 'test', '1 Laki, 1 Perempuan', 'sdasda', 'image4.jpg');
 
 -- --------------------------------------------------------
 
@@ -58,15 +75,27 @@ CREATE TABLE `paket` (
 --
 
 CREATE TABLE `pemesanan` (
-  `id_pemesanan` int(11) NOT NULL,
+  `id_pemesanan` int(255) NOT NULL,
   `catatan` varchar(255) DEFAULT NULL,
   `id_paket` int(11) NOT NULL,
   `tgl_pesan` date NOT NULL,
   `tgl_kembali` date NOT NULL,
-  `status` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'Waiting',
   `lokasi` varchar(255) NOT NULL,
+  `bukti_pembayaran` varchar(255) DEFAULT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pemesanan`
+--
+
+INSERT INTO `pemesanan` (`id_pemesanan`, `catatan`, `id_paket`, `tgl_pesan`, `tgl_kembali`, `status`, `lokasi`, `bukti_pembayaran`, `user_id`) VALUES
+(8, 'sdfsdfsdfsdfsdf', 11, '2011-08-19', '2011-08-19', 'Waiting', 'sdsdf', 'image1.jpg', 3),
+(9, 'dsfsdfsdf', 11, '2011-08-19', '2011-08-19', 'Waiting', 'sdfsdf', 'image5.jpg', 3),
+(10, 'sdcsdcdacvfdvsdvsdv', 11, '2011-08-19', '2011-08-19', 'Waiting', 'ascsdc', NULL, 3),
+(11, 'asdasdasc', 11, '2019-11-24', '2019-11-25', 'Waiting', 'Gedung Aula Pertamina', NULL, 4),
+(12, 'asxascasadasd', 12, '2019-11-24', '2019-11-27', 'Waiting', 'sdsdf', NULL, 3);
 
 -- --------------------------------------------------------
 
@@ -76,7 +105,6 @@ CREATE TABLE `pemesanan` (
 
 CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
-  `level` tinyint(4) NOT NULL DEFAULT 2,
   `type` enum('admin','customer') NOT NULL DEFAULT 'customer',
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
@@ -87,15 +115,23 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `type`, `username`, `password`, `nama`, `alamat`, `no_telp`, `email`) VALUES
+(2, 'customer', 'test', 'test', 'test', 'testinng', '232323', 'test@gmail.com'),
+(3, 'admin', 'admin', 'admin', 'admin', NULL, NULL, NULL),
+(4, 'customer', 'taufik', '1234', 'Taufik', 'Jalan jalan', '089928812992', 'taufikprayitno16@gmail.com');
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `galeri`
+-- Indexes for table `galeri2`
 --
-ALTER TABLE `galeri`
-  ADD PRIMARY KEY (`id_gambar`,`paket_id`),
-  ADD KEY `fk_galeri_paket1_idx` (`paket_id`);
+ALTER TABLE `galeri2`
+  ADD PRIMARY KEY (`id_gambar`);
 
 --
 -- Indexes for table `paket`
@@ -122,26 +158,32 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `galeri2`
+--
+ALTER TABLE `galeri2`
+  MODIFY `id_gambar` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `paket`
 --
 ALTER TABLE `paket`
-  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  MODIFY `id_pemesanan` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `galeri`
---
-ALTER TABLE `galeri`
-  ADD CONSTRAINT `fk_galeri_paket1` FOREIGN KEY (`paket_id`) REFERENCES `paket` (`id_paket`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `pemesanan`
