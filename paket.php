1f67af6f465215d7dee0f9d828a905c69578e8a7
DenatanBFS <denatanbagusfs@gmail.com>
2019-11-26 12:44:01 +0700

    card paket

--- paket.php
+++ paket.php
@@ -51,12 +51,12 @@
                         $HitungFree = 0;
                     }                   
                     ?>
-                    <div class="col-lg-4 col-md-6">
+                    <div class="col-lg-3 col-md-5">
                     <!--Panel-->
-                    <div class="card">
+                    <div class="card" style="background-color: rgba(0, 0, 0, 0.37)">
                         <h5 class="text-uppercase font-weight-bold text-center pt-4 text-white"> <span class="bg-ungu1 py-2 px-3 rounded"><?php echo $row["nama_paket"] ?></span> </h5>
                         <div class="card-body">
-                            <h5 class="text-center font-weight-bold mb-3">Rp.<?php echo $row["harga"] ?>,-</h5>
+                            <h5 class="text-center font-weight-bold mb-3 text-white">Rp.<?php echo $row["harga"] ?>,-</h5>
                             <ul class="list-group">
                             <li class="list-group-item d-flex justify-content-between align-items-center">
                                 Dekorasi
