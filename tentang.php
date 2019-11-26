f457a3f760a6f082ee15c79e18a2fa283996aa11
DenatanBFS <denatanbagusfs@gmail.com>
2019-11-26 17:24:35 +0700

    coba

--- tentang.php
+++ tentang.php
@@ -34,10 +34,10 @@
             <div class="row">
             <div class="col-6 pr-5">
                 <div class="column">
-                    <h3 style="color:rgb(255, 166, 109)">Tentang Cintya Wedding Organizer</h3>
+                    <h3 style="color:purple">Tentang Cintya Wedding Organizer</h3>
                     <p>Cintya wedding organizer merupakan salah satu upaya dalam membantu menyiapkan pesta pernikahan yang yang memakai web sebagai media promosi. Disini kami menyediakan berbagai paket pernikahan dengan harga terjangkau dan pelayanan yang bagus.</p>
                     <br>
-                    <h3 style="color:rgb(255, 166, 109)">Kontak Kami</h3>
+                    <h3 style="color:purple">Kontak Kami</h3>
                     <p>
                     <i class="far fa-envelope"></i> Cintyawedding@gmail.com
                     </p>
@@ -67,7 +67,7 @@
                 </div>
             </div>
             <div class="col-6 pl-5">
-                <h3 style="color:rgb(255, 166, 109)">Alamat</h3>
+                <h3 style="color:purple">Alamat</h3>
                     <p>Jl. Kapt. Kyai Ilyas 31A Lumajang</p>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.6962962683892!2d113.21658041428591!3d-8.132369494145893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6678a585d5db3%3A0x4598dffeffb08eb4!2sJl.%20Kapten%20Kyai%20Ilyas%2C%20Kec.%20Lumajang%2C%20Kabupaten%20Lumajang%2C%20Jawa%20Timur%2067316!5e0!3m2!1sid!2sid!4v1572189969157!5m2!1sid!2sid"
                     height="580" width="550" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
